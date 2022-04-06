2cede6cbbf02d4c84e39dfc1f99b25a79989cd02
Pijamo <ngulliejake@gmail.com>
2022-04-07 00:33:24 +0530

    test route

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mysql = require('mysql');
 var cors = require('cors')
 
 const routes = require('./routes')
+const configs = require('./config')
 const port = process.env.PORT || 3000
 
 const app = express();
@@ -12,6 +13,9 @@ app.use(cors({ credentials: true, origin: ['http://localhost:3000'] }));
 
 // Route 1 - register as GET 
 app.get('/', routes.hello)
+
+// Route 2 - Test Route
+app.get('/test', routes.test)
 
 app.listen(port, () => {
     console.log(`Server running at ${port}`);
