4222d1fa562ee955c9c686eb3e88545dbac50c86
Pijamo <ngulliejake@gmail.com>
2022-04-06 23:23:53 +0530

    make it better

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors({ credentials: true, origin: ['http://localhost:3000'] }));
 
 // Route 1 - register as GET 
-app.get('/hello', routes.hello)
+app.get('/', routes.hello)
 
 app.listen(port, () => {
     console.log(`Server running at ${port}`);
