ca23a536892b95e93ae85b8723653d5177064365
Pijamo <ngulliejake@gmail.com>
2022-04-07 00:33:37 +0530

    cmmot

--- routes.js
+++ routes.js
@@ -1,13 +1,18 @@
 const config = require('./config.json')
 const mysql = require('mysql');
 const e = require('express');
+const { connect, param } = require("./server")
 
+const connection = mysql.createConnection({
+    host: config.rds_host,
+    user: config.rds_user,
+    password: config.rds_password,
+    port: config.rds_port,
+    database: config.rds_db
+});
+connection.connect();
 
-// ********************************************
-//            SIMPLE ROUTE EXAMPLE
-// ********************************************
-
-// Route 1 (handler)
+// Route 1 (test)
 async function hello(req, res) {
     // a GET request to /hello?name=Steve
     if (req.query.name) {
@@ -15,6 +20,22 @@ async function hello(req, res) {
     } else {
         res.send(`Hello! Welcome to the SeaQuill server!`)
     }
+}
+
+// Route 2 (test)
+async function test(req, res) {
+
+    var query = `
+    SELECT *
+    FROM States
+    `
+
+    connection.query(query, function(err, results, fields) {
+        if (err) console.log(err);
+        else {
+            res.json({ results: results })
+        }
+        })
 }
 
 
