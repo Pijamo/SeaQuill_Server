078cc98513c87c6b2040a67c36ddd423169a175d
Alex Vena <alexvena@seas.upenn.edu>
2022-04-16 15:57:56 -0600

    Added routes 1 and 2 to Server API

--- routes.js
+++ routes.js
@@ -1,14 +1,15 @@
 const config = require('./config.json')
 const mysql = require('mysql');
 const e = require('express');
-const { connect, param } = require("./server")
+const { connect, param, request } = require("./server")
 
 const connection = mysql.createConnection({
     host: config.rds_host,
     user: config.rds_user,
     password: config.rds_password,
     port: config.rds_port,
-    database: config.rds_db
+    database: config.rds_db,
+    multipleStatements: true
 });
 connection.connect();
 
@@ -25,17 +26,152 @@ async function hello(req, res) {
         console.log(string)
         res.send(string)
     }
+    
+}
+
+// Route 1 (counties)
+async function counties(req, res) {
+
+    const page = req.query.page;
+    const pagesize = req.query.pagesize ? req.query.pagesize: 10;
+
+    const education =parseInt(req.query.education);
+    const freedom = parseInt(req.query.freedom);
+    const safety= parseInt(req.query.safety);
+    const social =parseInt(req.query.social);
+    const business = parseInt(req.query.business);
+    const economic= parseInt(req.query.economic);
+    const infrastructure =parseInt(req.query.infrastructure);
+    const governance = parseInt(req.query.governance);
+    const health= parseInt(req.query.health);
+    const living = parseInt(req.query.living);
+    const environment= parseInt(req.query.environment);
+    const total = education + freedom + safety + social + business + economic + 
+        infrastructure + governance + health + living + environment;
+    
+    const zip = req.query.zip;
+
+    var create_view = `DROP VIEW IF EXISTS AdjustedScores;
+
+    CREATE VIEW AdjustedScores(county_id, county_name, county_state, metric, adj_score) AS
+(SELECT C.fips_code, C.name, C.state_id, metric, score_2021 * ${education} AS adj_score
+FROM Prosperity P JOIN Counties C ON P.county_id = C.fips_code
+WHERE metric = 'Education')
+
+UNION
+
+(SELECT C.fips_code, C.name, C.state_id, metric, score_2021  * ${freedom} AS adj_score
+FROM Prosperity P JOIN Counties C ON P.county_id = C.fips_code
+WHERE metric = 'Personal Freedom')
+
+UNION
+
+(SELECT C.fips_code, C.name, C.state_id, metric, score_2021 * ${safety} AS adj_score
+FROM Prosperity P JOIN Counties C ON P.county_id = C.fips_code
+WHERE metric = 'Safety and Security')
+
+UNION
+
+(SELECT C.fips_code, C.name, C.state_id, metric, score_2021  * ${social} AS adj_score
+FROM Prosperity P JOIN Counties C ON P.county_id = C.fips_code
+WHERE metric = 'Social Capital')
+
+UNION
+
+(SELECT C.fips_code, C.name, C.state_id, metric, score_2021  * ${business} AS adj_score
+FROM Prosperity P JOIN Counties C ON P.county_id = C.fips_code
+WHERE metric = 'Business Environment')
+
+UNION
+
+(SELECT C.fips_code, C.name, C.state_id, metric, score_2021 * ${economic} AS adj_score
+FROM Prosperity P JOIN Counties C ON P.county_id = C.fips_code
+WHERE metric = 'Economic Quality')
+
+UNION
+
+(SELECT C.fips_code, C.name, C.state_id, metric, score_2021  * ${infrastructure} AS adj_score
+FROM Prosperity P JOIN Counties C ON P.county_id = C.fips_code
+WHERE metric = 'Infrastructure')
+
+UNION
+
+(SELECT C.fips_code, C.name, C.state_id, metric, score_2021  * ${governance} AS adj_score
+FROM Prosperity P JOIN Counties C ON P.county_id = C.fips_code
+WHERE metric = 'Governance')
+
+UNION
+
+(SELECT C.fips_code, C.name, C.state_id, metric, score_2021  * ${health} AS adj_score
+FROM Prosperity P JOIN Counties C ON P.county_id = C.fips_code
+WHERE metric = 'Health')
+
+UNION
+
+(SELECT C.fips_code, C.name, C.state_id, metric, score_2021  * ${living} AS adj_score
+FROM Prosperity P JOIN Counties C ON P.county_id = C.fips_code
+WHERE metric = 'Living Conditions')
+
+UNION
+
+(SELECT C.fips_code, C.name, C.state_id, metric, score_2021  * ${environment} AS adj_score
+FROM Prosperity P JOIN Counties C ON P.county_id = C.fips_code
+WHERE metric = 'Natural Environment');`;
+
+var recommendations;
+if (zip){
+    recommendations = `DROP TABLE IF EXISTS Recommendations;
+    CREATE TEMPORARY TABLE Recommendations
+    SELECT A.county_id, A.county_name, A.county_state, SUM(adj_score) /  ${total} AS total_score
+    FROM AdjustedScores A
+    GROUP BY A.county_id, A.county_name, A.county_state
+    HAVING total_score >= (SELECT SUM(adj_score) /  ${total} AS current_score
+                            FROM AdjustedScores NATURAL JOIN Districts
+                            WHERE zip = ${zip})
+    ORDER BY total_score DESC;
+    
+    SELECT * FROM Recommendations
+    LIMIT ${pagesize};`
+}
+else{
+    recommendations = `DROP TABLE IF EXISTS Recommendations;
+    CREATE TEMPORARY TABLE Recommendations
+    SELECT county_id, county_name, county_state, SUM(adj_score) /  ${total} AS total_score
+    FROM AdjustedScores
+    GROUP BY county_id, county_name, county_state
+    ORDER BY total_score DESC;
+    
+    SELECT * FROM Recommendations
+    LIMIT ${pagesize};`
+}
+
+    connection.query(create_view + recommendations, function(error, results){
+        if (error){
+            console.log(error)
+            res.json({ error: error})
+        } else {
+            res.json({ results: results[4]})
+        }
+    })
 }
 
 
-// Route (test)
-async function test(req, res) {
+// ********************************************
+//            SEARCH PAGES ROUTES
+// ********************************************
 
-    var query2 = `SELECT *
-    FROM States
-    `
+//Route 2: Get Cities and States based on partially or fully completed city input
 
-    connection.query(query2, function(error, results){
+async function searchCity(req, res) {
+
+    const city = req.query.city
+
+    query = `SELECT DISTINCT D.city, S.name
+    FROM States S JOIN Counties C ON S.id = C.state_id
+    JOIN Districts D ON C.fips_code = D.county_id
+    WHERE D.city LIKE '%${city}%' ;`
+
+    connection.query(query, function(error, results){
         if (error){
             console.log(error)
             res.json({ error: error})
@@ -45,20 +181,9 @@ async function test(req, res) {
     })
 }
 
-// Route 1 (getCounties)
-async function getCounties(req, res) {
-
-}
-
-
-// ********************************************
-//            SEARCH PAGES ROUTES
-// ********************************************
-
-//Route 2: Get State List
-
-
 //Route 3: Get property List
+
+
 
 
 // ********************************************
@@ -81,6 +206,5 @@ async function getCounties(req, res) {
 
 
 module.exports = {
-    hello,
-    test
+    hello, counties, searchCity
 }
--- server.js
+++ server.js
@@ -4,18 +4,21 @@ var cors = require('cors')
 
 const routes = require('./routes')
 const configs = require('./config')
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 
 const app = express();
 
-// whitelist localhost 3000
+// whitelist localhost 8080
 app.use(cors({ credentials: true, origin: ['http://localhost:3000'] }));
 
 // Route 1 - register as GET 
 app.get('/', routes.hello)
 
-// Route 2 - Test Route
-app.get('/test', routes.test)
+// Route 1- Recommended counties
+app.get('/counties', routes.counties)
+
+// Route 2 - City, State based on search input
+app.get('/searchCity', routes.searchCity)
 
 app.listen(port, () => {
     console.log(`Server running at ${port}`);
